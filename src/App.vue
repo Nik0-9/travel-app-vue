01f54cba00381a8ab99ca1bdaba248e578c9c732
Nik0-9 <filannino.nicola9@gmail.com>
2024-08-05 20:39:54 +0200

    added list of trip in Home.vue

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,35 @@
 </template>
 
 <script>
-import {store} from './store.js';
+import { store } from './store.js';
 import axios from 'axios';
 import HeaderComponent from './components/HeaderComponent.vue';
-  export default {
-    name: 'App',
-    components: {
-      HeaderComponent
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  },
+  data() {
+    return {
+      store
     }
+  },
+  methods: {
+    async fetchTrips() {
+      try {
+        const response = await axios.get(`${store.apiBaseUrl}trips`);
+        store.trips = response.data.results;
+        console.log(store.trips);
+
+      } catch (error) {
+        console.error('Error fetching trips:', error);
+      }
+    }
+  },
+  mounted() {
+    this.fetchTrips();
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
